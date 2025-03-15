5d77c5b08118bdd45960bbd9fdfe94aec8a30f80
Nikhil-sha <nikhil.sharma.main@gmail.com>
2025-03-15 18:00:57 +0530

    update header.js

--- components/header.js
+++ components/header.js
@@ -11,16 +11,20 @@ class Header extends Component {
 
 	render() {
 		const { handleAsideToggle } = this.context;
-		const isHome = this.props.location.pathname === '/';
-		console.log('Current Path:', this.props.location.pathname);
+		const currRoute = this.props.location.pathname.split("/")[1].slice(0, 1).toUpperCase() + this.props.location.pathname.split("/")[1].slice(1);
 
 		return (
 			<header className="w-full flex justify-between items-center bg-neutral-50 border-b border-neutral-200 rounded-b-xl px-3 py-3.5">
 				<div>
-					{!isHome ?
-						<button className="fade_in group size-8 flex justify-center items-center rounded-full bg-neutral-100 hover:bg-yellow-400 transition-all" onClick={this.navigateBack} aria-label="go back">
-							<i className="fas fa-chevron-left text-neutral-700 group-hover:text-neutral-100"></i>
-						</button>
+					{currRoute !== "" ?
+						<div className="flex gap-2 items-center">
+							<button className="fade_in group flex justify-center items-center" onClick={this.navigateBack} aria-label="go back">
+								<i className="fas fa-circle-chevron-left text-lg text-neutral-600 group-hover:text-yellow-400 mr-1.5"></i>
+								<h1 className="fade_in text-xl font-black text-neutral-700">
+									{currRoute}
+								</h1>
+							</button>
+						</div>
 					:
 					<Link to="/">
 							<h1 className="fade_in text-xl font-black text-neutral-700 flex items-center">
